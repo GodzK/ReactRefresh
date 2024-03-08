d0ccb460ce2668aba319b94481b8c375e6160c4f
Phakaphol <64467457+GodzK@users.noreply.github.com>
2024-03-08 16:05:32 +0700

    the Goat make dream
    add a from acion and make state that send the data post

--- react-crash-course/src/components/Postlist.jsx
+++ react-crash-course/src/components/Postlist.jsx
@@ -4,37 +4,29 @@ import Post from "./Post";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import styles from "./Postlist.module.css";
-function Postlist({isPosting , onStopPosting}) {
-  // enteredBody: เก็บ text ที่ใส่ใน NewPost component
-  // enteredAuthor: เก็บชื่อที่ใส่ใน NewPost component
- 
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState();
-  // stateใช้ติดตามค่าที่เปลี่ยนเเปลง
 
-  //  อัปเดต enteredBody state กับค่าที่ใส่ใน textarea
-  //  อัปเดต enteredAuthor state กับค่าที่ใส่ใน text input
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
+
+function Postlist({isPosting , onStopPosting}) {
+  const [posts , setPosts] = useState([]);
+
+function addPostHandler(postData){
+  // WHEN NEW STATE DEPEND ON OLD STATE
+ setPosts((existingPosts) =>[postData , ...existingPosts]);
+}
 
   return (
     <>
-    {isPosting ? (<Modal onClose ={onStopPosting}>
+    {isPosting && (
+    <Modal onClose ={onStopPosting}>
       <NewPost
-        onBodyChange={bodyChangeHandler}
-        onAuthorChange={authorChangeHandler}
+        onCancel ={onStopPosting}
+        onAddPost={addPostHandler}
       />
     </Modal>
-    ) : false }
-    
-      
-      <ul className={styles.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
-        <Post author="Jake" body="Next Js is The Best!" />
+    )}
+      <ul className={styles.posts}> 
+      {/* map return new array */}
+      {posts.map((post)=><Post author={post.author} body={post.body} />)} 
       </ul>
     </>
   );
